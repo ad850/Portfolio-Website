750e8381623ce8062dff2e2034bcea7a01f7674a
Aditya Sharma <83112843+ad850@users.noreply.github.com>
2022-01-29 19:44:30 +0530

    Update Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -11,41 +11,27 @@ const Topbar = ({ menuopen, setmenuOpen }) => {
         <div className={"topbar " + (menuopen && "active")} >
 
             <div className="wrapper">
-
-
                 <div className="left">
-
                     <a href="#intro" className="logo"> {"< Aditya />"} </a>
-
                     <div className="itemcontainer">
-
                         <PersonIcon className="icon" />
-
                         <span> +91-8505085063</span>
-
                     </div>
                     <div className="itemcontainer">
-
                         <EmailIcon className="icon" />
-
                         <span> Aditya.sharma9827.as@gmail.com</span>
                         <a href="https://github.com/ad850" target="_blank" id="git" > <GitHubIcon  style={{color:"gray"}}/></a>
                         <a href="https://linkedin.com/in/aditya-sharma-986a84a2/" target="_blank" id="link" > <LinkedInIcon style={{color:"gray"}}/></a>
-
                     </div>
                 </div>
                 <div className="right">
-
                     <div className="hamburger" onClick={() => setmenuOpen(!menuopen)} >
-
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
-
                     </div>
                 </div>
             </div>
-
         </div >
     )
 }
