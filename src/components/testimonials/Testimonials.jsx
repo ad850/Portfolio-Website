9d91874962f576e67f25471bd473c267115023f3
Aditya Sharma <83112843+ad850@users.noreply.github.com>
2022-01-29 19:35:50 +0530

    Update Testimonials.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './testimonials.scss';
-
 import github from '../images/github-dots.svg'
-
 import html from '../images/html.svg'
 import javascript from '../images/javascript.svg'
 import materialui from '../images/materialui.svg'
@@ -16,12 +14,8 @@ import gitt from '../images/gitt.svg'
 const Testimonials = () => {
     return (
         <div className="testimonials" id="testimonials">
-
             <div className="heading">
-
-
                 <h1>My Tech Stack</h1>
-
             </div>
 
             <div className="wrapper">
@@ -32,10 +26,7 @@ const Testimonials = () => {
 
                 </div>
 
-
                 <div className="right">
-
-
                     <div className="item"><img src={html} alt="google" /> </div>
                     <div className="item"><img src={javascript} alt="google" /></div>
                     <div className="item"><img src={materialui} alt="google" /></div>
@@ -45,10 +36,7 @@ const Testimonials = () => {
                     <div className="item"><img src={redux} alt="google" /></div>
                     <div className="item"><img src={css} alt="google" /></div>
                     <div className="item"><img src={gitt} alt="google" /></div>
-
                 </div>
-
-
             </div>
         </div>
     )
