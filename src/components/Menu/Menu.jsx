bac4be3d3ecccd2593209620828776f17437fd5f
Aditya Sharma <83112843+ad850@users.noreply.github.com>
2022-02-09 17:44:01 +0530

    Update Menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import './Menu.scss';
-
-
 const Menu = ({ menuopen, setmenuOpen }) => {
     return (
         <div className={"menu " + (menuopen && "active")}>
-
             <ul onClick={() => { setmenuOpen(false) }}>
                 <li ><a href="#intro">Home</a></li>
                 <li ><a href="#portfolio">Portfolio</a></li>
